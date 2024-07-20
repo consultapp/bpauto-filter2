372fbd2d9731849d81b8f513083da49e69c356ca
Dmitry Egorov <info@consultapp.ru>
2024-07-20 20:24:58 +0300

    yandex

--- src/components/HeaderFilter/HeaderFilter.tsx
+++ src/components/HeaderFilter/HeaderFilter.tsx
@@ -4,14 +4,8 @@ import TabsController from "@/components/TabsController/TabsController";
 import SelectedTab from "../SelectedTab/SelectedTab";
 
 import styles from "./style.module.scss";
-import { useEffect } from "react";
-import { yandexSearchScript } from "@/functions/yandexScript";
 
 export default function HeaderFilter() {
-  useEffect(() => {
-    yandexSearchScript();
-  }, []);
-
   return (
     <div className={styles.root}>
       <div className={styles.logo}>
--- src/components/TabYandexSearch/TabYandexSearch.tsx
+++ src/components/TabYandexSearch/TabYandexSearch.tsx
@@ -1,34 +1,20 @@
-import classNames from "classnames";
-import styles from "./style.module.scss";
-
 export default function TabYandexSearch() {
   return (
     <div
-      className={classNames(
-        "ya-site-form",
-        "ya-site-form_inited_no",
-        styles.ya_site_form,
-        styles.ya_site_form_inited_no,
-        styles.root
-      )}
-      data-bem={`&quot;action&quot;:&quot;https://bpauto.ru/yandex-search-goods/&quot;,&quot;arrow&quot;:false,&quot;bg&quot;:&quot;transparent&quot;,&quot;fontsize&quot;:&quot;15&quot;,&quot;fg&quot;:&quot;#000000&quot;,&quot;language&quot;:&quot;ru&quot;,&quot;logo&quot;:&quot;rb&quot;,&quot;publicname&quot;:&quot;&quot;,&quot;suggest&quot;:true,&quot;target&quot;:&quot;_self&quot;,&quot;tld&quot;:&quot;ru&quot;,&quot;type&quot;:2,&quot;usebigdictionary&quot;:false,&quot;searchid&quot;:6977498,&quot;input_fg&quot;:&quot;#000000&quot;,&quot;input_bg&quot;:&quot;#FFFFFF&quot;,&quot;input_fontStyle&quot;:&quot;normal&quot;,&quot;input_fontWeight&quot;:&quot;normal&quot;,&quot;input_placeholder&quot;:&quot;&quot;,&quot;input_placeholderColor&quot;:&quot;#000000&quot;,&quot;input_borderColor&quot;:&quot;#cbcbcb&quot;,&quot;is_catalog&quot;:true`}
+      className="ya-site-form ya-site-form_inited_no"
+      data-bem={`&quot;action&quot;:&quot;https://bpauto.ru/yandex-search/&quot;,&quot;arrow&quot;:false,&quot;bg&quot;:&quot;#339999&quot;,&quot;fontsize&quot;:14,&quot;fg&quot;:&quot;#000000&quot;,&quot;language&quot;:&quot;ru&quot;,&quot;logo&quot;:&quot;rb&quot;,&quot;publicname&quot;:&quot;Поиск по bpauto.ru&quot;,&quot;suggest&quot;:true,&quot;target&quot;:&quot;_self&quot;,&quot;tld&quot;:&quot;ru&quot;,&quot;type&quot;:2,&quot;usebigdictionary&quot;:true,&quot;searchid&quot;:6863453,&quot;input_fg&quot;:&quot;#000000&quot;,&quot;input_bg&quot;:&quot;#ffffff&quot;,&quot;input_fontStyle&quot;:&quot;normal&quot;,&quot;input_fontWeight&quot;:&quot;normal&quot;,&quot;input_placeholder&quot;:&quot;введите запрос&quot;,&quot;input_placeholderColor&quot;:&quot;#999999&quot;,&quot;input_borderColor&quot;:&quot;#dee9eb&quot;`}
     >
       <form
-        action="//site.yandex.ru/search/site/catalog/"
+        action="https://yandex.ru/search/site/"
         method="get"
         target="_self"
-        acceptCharset="utf-8"
+        accept-charset="utf-8"
       >
-        <input type="hidden" name="searchid" value="6977498" />
+        <input type="hidden" name="searchid" value="6863453" />
         <input type="hidden" name="l10n" value="ru" />
         <input type="hidden" name="reqenc" value="" />
-        <input
-          type="search"
-          placeholder="введите запрос"
-          name="text"
-          className={styles.input_s}
-        />
-        <input type="submit" value="Найти" className={styles.input_b} />
+        <input type="search" style={{ height: "38px" }} name="text" value="" />
+        <input type="submit" value="Найти" style={{ height: "38px" }} />
       </form>
     </div>
   );
