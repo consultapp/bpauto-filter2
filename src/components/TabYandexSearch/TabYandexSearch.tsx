1402dc6433f0c4b6416b8858c3274b109c97cf2c
Dmitry Egorov <info@consultapp.ru>
2024-07-20 22:31:26 +0300

    yandex

--- src/components/TabYandexSearch/TabYandexSearch.tsx
+++ src/components/TabYandexSearch/TabYandexSearch.tsx
@@ -1,10 +1,21 @@
 import { SITE_URL } from "@/fixtures/consts";
+import { memo, useEffect, useState } from "react";
 
-export default function TabYandexSearch() {
-  return (
-    <iframe
-      width="100%"
-      src={SITE_URL + "/yandex-search-goods/iframe.html"}
-    ></iframe>
-  );
-}
+const TabYandexSearch = memo(function TabYandexSearch() {
+  const [htmlFileString, setHtmlFileString] = useState<string>();
+
+  async function fetchHtml() {
+    fetch(`${SITE_URL}/yandex-search-goods/iframe.html`)
+      .then((data) => data.text())
+      .then((text) => {
+        setHtmlFileString(text);
+      });
+  }
+  useEffect(() => {
+    fetchHtml();
+  }, []);
+
+  return <div dangerouslySetInnerHTML={{ __html: htmlFileString ?? "" }}></div>;
+});
+
+export default TabYandexSearch;
